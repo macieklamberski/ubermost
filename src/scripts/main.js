51c681cbd5a0c134277a257963f520c0cf5419e9
Maciej Lamberski <maciej@lamberski.com>
2015-12-21 07:45:32 +0100

    Create Posts overlay

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+//= include ../../bower_components/jquery/dist/jquery.js
+
+(function ($) {
+
+  var App = {
+
+    /**
+     * Initialize features
+     */
+    init: function () {
+      App.feature();
+    },
+
+    /**
+     * Custom feature
+     */
+    feature: function () {
+
+    }
+  };
+
+  $(function () {
+    App.init();
+  });
+
+})(jQuery);
