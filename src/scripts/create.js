888196a7d4a86d9914b0e513314c5f1962aec46a
Maciej Lamberski <maciej@lamberski.com>
2017-03-25 07:34:04 +0100

    Reindent lines and stuff

--- src/scripts/create.js
+++ src/scripts/create.js
@@ -4,317 +4,317 @@ var ImagesLoaded = require('imagesloaded')
 var Handlebars = require('handlebars')
 
 var Helper = {
-  compileTemplate: function (id, data) {
-    var template = Handlebars.compile($('#' + id + '-template').html())
-    return template(data)
-  },
+    compileTemplate: function (id, data) {
+        var template = Handlebars.compile($('#' + id + '-template').html())
+        return template(data)
+    },
 
-  openOverlay: function ($overlay, callback) {
-    Application.$BODY.css('overflow', 'hidden')
-    $overlay.addClass('overlay--open')
-    $overlay.find('.overlay__close').on('click', function (event) {
-      Helper.closeOverlay($overlay)
-      event.preventDefault()
-    })
+    openOverlay: function ($overlay, callback) {
+        Application.$BODY.css('overflow', 'hidden')
+        $overlay.addClass('overlay--open')
+        $overlay.find('.overlay__close').on('click', function (event) {
+            Helper.closeOverlay($overlay)
+            event.preventDefault()
+        })
 
-    if (callback !== undefined) {
-      callback($overlay)
+        if (callback !== undefined) {
+            callback($overlay)
+        }
+    },
+
+    closeOverlay: function ($overlay) {
+        if (!$overlay.length) {
+            return;
+        }
+        Application.$BODY.css('overflow', 'auto')
+        $overlay.removeClass('overlay--open')
+    },
+
+    startLoading: function ($loading) {
+        $loading.removeClass('loading--done')
+    },
+
+    stopLoading: function ($loading) {
+        $loading.addClass('loading--done')
     }
-  },
-
-  closeOverlay: function ($overlay) {
-    if (!$overlay.length) {
-      return;
-    }
-    Application.$BODY.css('overflow', 'auto')
-    $overlay.removeClass('overlay--open')
-  },
-
-  startLoading: function ($loading) {
-    $loading.removeClass('loading--done')
-  },
-
-  stopLoading: function ($loading) {
-    $loading.addClass('loading--done')
-  }
 }
 
 var Sizes = {
-  $trigger: $('.dropdown'),
-  $selected: $('.dropdown span'),
-  $overlay: $('#sizes-overlay'),
-  $input: $('form input[name="size_id"]'),
+    $trigger: $('.dropdown'),
+    $selected: $('.dropdown span'),
+    $overlay: $('#sizes-overlay'),
+    $input: $('form input[name="size_id"]'),
 
-  init: function () {
-    Sizes.bindOpenOverlay()
-    Sizes.bindSelectSize()
+    init: function () {
+        Sizes.bindOpenOverlay()
+        Sizes.bindSelectSize()
 
-    if (!Sizes.$input.val()) {
-      Sizes.detectInitialResolution()
-    }
-  },
-
-  bindOpenOverlay: function () {
-    Sizes.$trigger.on('click', function (event) {
-      Helper.openOverlay(Sizes.$overlay)
-      event.preventDefault()
-    })
-  },
-
-  bindSelectSize: function () {
-    Sizes.$overlay.find('label').on('click', function (event) {
-      var $self = $(this)
-      Sizes.$input.val($('input', $self).val())
-      Sizes.$selected.html($('span', $self).html())
-      Helper.closeOverlay(Sizes.$overlay)
-      event.preventDefault()
-    })
-  },
-
-  detectInitialResolution: function () {
-    var screenX = screen.width;
-    var screenY = screen.height;
-    var $matched = null;
-
-    // Check if there's option with exact size.
-    $('label[data-width="' + screenX + '"][data-height="' + screenY + '"]').each(function () {
-      $matched = $(this)
-    })
-
-    // Check if there's option with the same ratio.
-    if (!$matched) {
-      var $sizes = $('label[data-ratio="' + (screenX / screenY).toFixed(1) + '"]')
-
-      $sizes.each(function () {
-        var $self = $(this)
-        if ($self.data('width') > screenX && $self.data('height') > screenY) {
-          if (!$matched || $self.data('width') < $matched.data('width') && $self.data('height') < $matched.data('height')) {
-            $matched = $self;
-          }
+        if (!Sizes.$input.val()) {
+            Sizes.detectInitialResolution()
         }
-      })
-    }
+    },
 
-    // Match any resolution that is bigger.
-    if (!$matched) {
-      var $sizes = $('label[data-ratio]')
+    bindOpenOverlay: function () {
+        Sizes.$trigger.on('click', function (event) {
+            Helper.openOverlay(Sizes.$overlay)
+            event.preventDefault()
+        })
+    },
 
-      $sizes.each(function () {
-        var $self = $(this)
-        if ($self.data('width') > screenX && $self.data('height') > screenY) {
-          if (!$matched || $self.data('width') < $matched.data('width') && $self.data('height') < $matched.data('height')) {
-            $matched = $self;
-          }
+    bindSelectSize: function () {
+        Sizes.$overlay.find('label').on('click', function (event) {
+            var $self = $(this)
+            Sizes.$input.val($('input', $self).val())
+            Sizes.$selected.html($('span', $self).html())
+            Helper.closeOverlay(Sizes.$overlay)
+            event.preventDefault()
+        })
+    },
+
+    detectInitialResolution: function () {
+        var screenX = screen.width;
+        var screenY = screen.height;
+        var $matched = null;
+
+        // Check if there's option with exact size.
+        $('label[data-width="' + screenX + '"][data-height="' + screenY + '"]').each(function () {
+            $matched = $(this)
+        })
+
+        // Check if there's option with the same ratio.
+        if (!$matched) {
+            var $sizes = $('label[data-ratio="' + (screenX / screenY).toFixed(1) + '"]')
+
+            $sizes.each(function () {
+                var $self = $(this)
+                if ($self.data('width') > screenX && $self.data('height') > screenY) {
+                    if (!$matched || $self.data('width') < $matched.data('width') && $self.data('height') < $matched.data('height')) {
+                        $matched = $self;
+                    }
+                }
+            })
         }
-      })
-    }
 
-    if ($matched) {
-      $matched.trigger('click')
-      Sizes.$selected.find('em').remove()
-      Sizes.$selected.append(Sizes.$selected.data('detected'))
+        // Match any resolution that is bigger.
+        if (!$matched) {
+            var $sizes = $('label[data-ratio]')
+
+            $sizes.each(function () {
+                var $self = $(this)
+                if ($self.data('width') > screenX && $self.data('height') > screenY) {
+                    if (!$matched || $self.data('width') < $matched.data('width') && $self.data('height') < $matched.data('height')) {
+                        $matched = $self;
+                    }
+                }
+            })
+        }
+
+        if ($matched) {
+            $matched.trigger('click')
+            Sizes.$selected.find('em').remove()
+            Sizes.$selected.append(Sizes.$selected.data('detected'))
+        }
     }
-  }
 }
 
 var Colors = {
-  $inputs: $('.colors input[type="radio"]'),
+    $inputs: $('.colors input[type="radio"]'),
 
-  init: function () {
-    Colors.bindSelectColor()
-  },
+    init: function () {
+        Colors.bindSelectColor()
+    },
 
-  getCurrentColorId: function () {
-    return Colors.$inputs.filter(':checked').val()
-  },
+    getCurrentColorId: function () {
+        return Colors.$inputs.filter(':checked').val()
+    },
 
-  bindSelectColor: function () {
-    Colors.$inputs.on('change', function (event) {
-      Preview.load(
-        Preview.getCurrentPostId(),
-        Colors.getCurrentColorId()
-      )
-      event.preventDefault()
-    })
-  }
+    bindSelectColor: function () {
+        Colors.$inputs.on('change', function (event) {
+            Preview.load(
+                Preview.getCurrentPostId(),
+                Colors.getCurrentColorId()
+                )
+            event.preventDefault()
+        })
+    }
 }
 
 var Posts = {
-  $container: $('.posts'),
-  $endpoint: $('.posts').data('endpoint'),
-  $action: $('.posts').data('action'),
-  $overlay: $('#posts-overlay'),
-  $loading: $('#posts-overlay'),
+    $container: $('.posts'),
+    $endpoint: $('.posts').data('endpoint'),
+    $action: $('.posts').data('action'),
+    $overlay: $('#posts-overlay'),
+    $loading: $('#posts-overlay'),
 
-  init: function () {
-    Posts.bindOpenOverlay()
-    Posts.bindSelectPost()
-  },
+    init: function () {
+        Posts.bindOpenOverlay()
+        Posts.bindSelectPost()
+    },
 
-  bindOpenOverlay: function () {
-    Application.$BODY.on('click', '.button--change', function (event) {
-      Helper.openOverlay(Posts.$overlay, Posts.load)
-      event.preventDefault()
-    })
-  },
+    bindOpenOverlay: function () {
+        Application.$BODY.on('click', '.button--change', function (event) {
+            Helper.openOverlay(Posts.$overlay, Posts.load)
+            event.preventDefault()
+        })
+    },
 
-  bindSelectPost: function () {
-    Posts.$container.on('click', 'a', function (event) {
-      Preview.load(
-        $(this).data('post-id'),
-        Colors.getCurrentColorId()
-      )
-      Helper.closeOverlay(Posts.$overlay)
-      event.preventDefault()
-    })
-  },
+    bindSelectPost: function () {
+        Posts.$container.on('click', 'a', function (event) {
+            Preview.load(
+                $(this).data('post-id'),
+                Colors.getCurrentColorId()
+                )
+            Helper.closeOverlay(Posts.$overlay)
+            event.preventDefault()
+        })
+    },
 
-  load: function ($overlay) {
-    if (Posts.$container.children().length) {
-      Posts.fadeInElements()
-      return;
+    load: function ($overlay) {
+        if (Posts.$container.children().length) {
+            Posts.fadeInElements()
+            return;
+        }
+
+        $.get(Posts.$endpoint, {action: Posts.$action}, function (result) {
+            if (result.data) {
+                var data = result.data;
+            } else {
+                return;
+            }
+
+            var html = Helper.compileTemplate('posts', data)
+            Posts.$container.html(html)
+            Posts.$container.find('img').css('opacity', 0)
+            ImagesLoaded(Posts.$container.get(0), function () {
+                Helper.stopLoading(Posts.$loading)
+                Posts.fadeInElements()
+            })
+        }, 'json')
+    },
+
+    fadeInElements: function () {
+        var images = Posts.$container.find('img')
+        images.css('opacity', 0)
+
+        Velocity(images.get(), 'fadeIn', {
+            duration: 500,
+            stagger: 12.5
+        })
     }
-
-    $.get(Posts.$endpoint, {action: Posts.$action}, function (result) {
-      if (result.data) {
-        var data = result.data;
-      } else {
-        return;
-      }
-
-      var html = Helper.compileTemplate('posts', data)
-      Posts.$container.html(html)
-      Posts.$container.find('img').css('opacity', 0)
-      ImagesLoaded(Posts.$container.get(0), function () {
-        Helper.stopLoading(Posts.$loading)
-        Posts.fadeInElements()
-      })
-    }, 'json')
-  },
-
-  fadeInElements: function () {
-    var images = Posts.$container.find('img')
-    images.css('opacity', 0)
-
-    Velocity(images.get(), 'fadeIn', {
-      duration: 500,
-      stagger: 12.5
-    })
-  }
 }
 
 var Preview = {
-  $container: $('.preview'),
-  $share: $('.share'),
-  $endpoint: $('.preview').data('endpoint'),
-  $action: $('.preview').data('action'),
-  $loading: $('.preview').closest('.loading'),
-  $input: $('form input[name="post_id"]'),
+    $container: $('.preview'),
+    $share: $('.share'),
+    $endpoint: $('.preview').data('endpoint'),
+    $action: $('.preview').data('action'),
+    $loading: $('.preview').closest('.loading'),
+    $input: $('form input[name="post_id"]'),
 
-  init: function () {
-    Preview.load(
-      Preview.getCurrentPostId(),
-      Colors.getCurrentColorId()
-    )
-  },
+    init: function () {
+        Preview.load(
+            Preview.getCurrentPostId(),
+            Colors.getCurrentColorId()
+            )
+    },
 
-  getCurrentPostId: function () {
-    return Preview.$input.val()
-  },
+    getCurrentPostId: function () {
+        return Preview.$input.val()
+    },
 
-  load: function (postId, colorId) {
-    var loading = setTimeout(function () {
-      Helper.startLoading(Preview.$loading)
-    }, 250)
+    load: function (postId, colorId) {
+        var loading = setTimeout(function () {
+            Helper.startLoading(Preview.$loading)
+        }, 250)
 
-    Download.$trigger.attr('disabled', 'disabled')
+        Download.$trigger.attr('disabled', 'disabled')
 
-    $.get(Preview.$endpoint, {action: Preview.$action, post_id: postId, color_id: colorId}, function (result) {
-      if (result.data) {
-        var data = result.data;
-      } else {
-        return;
-      }
+        $.get(Preview.$endpoint, {action: Preview.$action, post_id: postId, color_id: colorId}, function (result) {
+            if (result.data) {
+                var data = result.data;
+            } else {
+                return;
+            }
 
-      Preview.$share.html(Helper.compileTemplate('share', data))
-      Preview.$share.addClass('share--enabled')
-      Download.$trigger.removeAttr('disabled')
+            Preview.$share.html(Helper.compileTemplate('share', data))
+            Preview.$share.addClass('share--enabled')
+            Download.$trigger.removeAttr('disabled')
 
-      Preview.$input.val(postId)
-      Preview.$container.html(Helper.compileTemplate('post', data))
-      Preview.$container.find('img').css('opacity', 0)
+            Preview.$input.val(postId)
+            Preview.$container.html(Helper.compileTemplate('post', data))
+            Preview.$container.find('img').css('opacity', 0)
 
-      ImagesLoaded(Preview.$container.get(0), function () {
-        clearTimeout(loading)
-        Helper.stopLoading(Preview.$loading)
+            ImagesLoaded(Preview.$container.get(0), function () {
+                clearTimeout(loading)
+                Helper.stopLoading(Preview.$loading)
 
-        Velocity(Preview.$container.find('img').get(), 'fadeIn', {
-          duration: 400,
-          complete: function () {
-            Preview.$container.css('background-image', 'url(' + data.image + ')')
-          }
-        })
-      })
-    }, 'json')
-  }
+                Velocity(Preview.$container.find('img').get(), 'fadeIn', {
+                    duration: 400,
+                    complete: function () {
+                        Preview.$container.css('background-image', 'url(' + data.image + ')')
+                    }
+                })
+            })
+        }, 'json')
+    }
 }
 
 var Download = {
-  $form: $('.main__options'),
-  $trigger: $('.button--download'),
+    $form: $('.main__options'),
+    $trigger: $('.button--download'),
 
-  init: function () {
-    Download.bindDownloadWallpaper()
-  },
+    init: function () {
+        Download.bindDownloadWallpaper()
+    },
 
-  bindDownloadWallpaper: function () {
-    Download.$form.on('submit', function (event) {
-      var self = this;
-      event.preventDefault()
-      setTimeout(function() { self.submit() }, 1)
-    })
-  }
+    bindDownloadWallpaper: function () {
+        Download.$form.on('submit', function (event) {
+            var self = this;
+            event.preventDefault()
+            setTimeout(function() { self.submit() }, 1)
+        })
+    }
 }
 
 var Keyboard = {
-  init: function () {
-    Keyboard.bindCloseOverlayOnEscape()
-  },
+    init: function () {
+        Keyboard.bindCloseOverlayOnEscape()
+    },
 
-  bindCloseOverlayOnEscape: function () {
-    Application.$DOCUMENT.on('keydown', function (event) {
-      if (event.keyCode == 27) {
-        Helper.closeOverlay($('.overlay--open'))
-      }
-    })
-  }
+    bindCloseOverlayOnEscape: function () {
+        Application.$DOCUMENT.on('keydown', function (event) {
+            if (event.keyCode == 27) {
+                Helper.closeOverlay($('.overlay--open'))
+            }
+        })
+    }
 }
 
 var ExternalLinks = {
-  init: function () {
-    var elements = [
-      '[rel="external"]'
-    ]
+    init: function () {
+        var elements = [
+            '[rel="external"]'
+        ]
 
-    $(elements.join()).attr('target', '_new')
-  }
+        $(elements.join()).attr('target', '_new')
+    }
 }
 
 var Application = {
-  $DOCUMENT: $(document),
-  $BODY: $('body'),
+    $DOCUMENT: $(document),
+    $BODY: $('body'),
 
-  init: function () {
-    Sizes.init()
-    Colors.init()
-    Posts.init()
-    Preview.init()
-    Download.init()
-    Keyboard.init()
-    ExternalLinks.init()
-  }
+    init: function () {
+        Sizes.init()
+        Colors.init()
+        Posts.init()
+        Preview.init()
+        Download.init()
+        Keyboard.init()
+        ExternalLinks.init()
+    }
 }
 
 $(function () {
-  Application.init()
+    Application.init()
 })
--- src/scripts/tumblr.js
+++ src/scripts/tumblr.js
@@ -1,78 +1,78 @@
 var $ = require('jquery')
 
 var BuyLinks = {
-  $quotes: $('[data-tags*="isbn"]'),
+    $quotes: $('[data-tags*="isbn"]'),
 
-  init: function () {
-    BuyLinks.getLinks()
-  },
+    init: function () {
+        BuyLinks.getLinks()
+    },
 
-  getISBNMap: function () {
-    var ISBNs = []
+    getISBNMap: function () {
+        var ISBNs = []
 
-    BuyLinks.$quotes.each(function () {
-      var $self = $(this)
+        BuyLinks.$quotes.each(function () {
+            var $self = $(this)
 
-      var tags = $self.data('tags').split(' ')
-      var isbn = tags.filter(function (item) {
-        return /^isbn/.test(item)
-      })
+            var tags = $self.data('tags').split(' ')
+            var isbn = tags.filter(function (item) {
+                return /^isbn/.test(item)
+            })
 
-      if (isbn.length > 0) {
-        $self.attr('data-isbn', isbn[0]);
-        ISBNs.push(isbn[0])
-      }
-    })
+            if (isbn.length > 0) {
+                $self.attr('data-isbn', isbn[0]);
+                ISBNs.push(isbn[0])
+            }
+        })
 
-    return ISBNs
-  },
+        return ISBNs
+    },
 
-  insertCTAs: function (links) {
-    BuyLinks.$quotes.each(function () {
-      var $self = $(this)
+    insertCTAs: function (links) {
+        BuyLinks.$quotes.each(function () {
+            var $self = $(this)
 
-      var link = links[$self.data('isbn')]
+            var link = links[$self.data('isbn')]
 
-      if (link) {
-        $self.find('footer cite')
-          .append('<span></span>')
-          .append('<a href="' + link + '">Buy the book</a>')
-      }
-    })
-  },
+            if (link) {
+                $self.find('footer cite')
+                .append('<span></span>')
+                .append('<a href="' + link + '">Buy the book</a>')
+            }
+        })
+    },
 
-  getLinks: function () {
-    var ISBNs = BuyLinks.getISBNMap()
-    var links = []
+    getLinks: function () {
+        var ISBNs = BuyLinks.getISBNMap()
+        var links = []
 
-    $.ajax({
-      url: 'http://create.ubermost.com/wp-admin/admin-ajax.php',
-      data: {
-        action: 'get_isbn_links',
-        isbns: ISBNs
-      },
-      dataType: 'json'
-    })
-    .done(function (result) {
-      links = result.data.links
-      BuyLinks.insertCTAs(links)
-      ExternalLinks.init()
-    })
-  }
+        $.ajax({
+            url: 'http://create.ubermost.com/wp-admin/admin-ajax.php',
+            data: {
+                action: 'get_isbn_links',
+                isbns: ISBNs
+            },
+            dataType: 'json'
+        })
+        .done(function (result) {
+            links = result.data.links
+            BuyLinks.insertCTAs(links)
+            ExternalLinks.init()
+        })
+    }
 }
 
 var ExternalLinks = {
-  init: function () {
-    var elements = [
-      '.post__text footer a',
-      '[rel="external"]'
-    ]
+    init: function () {
+        var elements = [
+            '.post__text footer a',
+            '[rel="external"]'
+        ]
 
-    $(elements.join()).attr('target', '_new')
-  }
+        $(elements.join()).attr('target', '_new')
+    }
 }
 
 $(function () {
-  BuyLinks.init()
-  ExternalLinks.init()
+    BuyLinks.init()
+    ExternalLinks.init()
 })
