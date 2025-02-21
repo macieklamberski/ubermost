1c8af875e3e8ee044635893de501e27200f810d3
Maciej Lamberski <maciej@lamberski.com>
2025-02-21 15:54:19 +0100

    Update Mikrob and install Marked to parse Markdown

--- views/partials/Copy.tsx
+++ views/partials/Copy.tsx
@@ -1,10 +1,13 @@
 import type { FC } from 'hono/jsx'
+import { marked } from 'marked'
 
 export type CopyProps = {
   body: string
 }
 
 export const Copy: FC<CopyProps> = ({ body }) => {
+  const markdown = marked(body, { async: false })
+
   // biome-ignore lint/security/noDangerouslySetInnerHtml: No need for that.
-  return <div class="copy" dangerouslySetInnerHTML={{ __html: body }} />
+  return <div className="copy" dangerouslySetInnerHTML={{ __html: markdown }} />
 }
